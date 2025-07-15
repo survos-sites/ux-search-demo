3a1691cec80de5942165852008d22fe0b8ecefed
Tac Tacelosky <tacman@gmail.com>
2025-07-14 21:00:11 -0400

    work with meili too

--- src/Search/JeopardySearch.php
+++ src/Search/JeopardySearch.php
@@ -5,12 +5,15 @@ namespace App\Search;
 use Mezcalito\UxSearchBundle\Attribute\AsSearch;
 use Mezcalito\UxSearchBundle\Search\AbstractSearch;
 
-#[AsSearch('Jeopardy', 'jeopardy')]
+#[AsSearch('dtdemo_Jeopardy', 'JeopardyMeiliSearch')]
 class JeopardySearch extends AbstractSearch
 {
     public function build(array $options = []): void
     {
-        $this->addFacet('category', 'Category');
+        $this
+            ->addFacet('category', 'Category')
+            ->addFacet('value', '$')
+        ;
         // ->addFacet('type', 'Type', null, ['limit' => 2])
         // ->addFacet('brand', 'Brand')
         // ->addFacet('rating', 'Rating')
